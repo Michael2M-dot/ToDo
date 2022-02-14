89075d8c7765ddce7f61f870c40c2196baf53b51
Michael2M <77737524+Michael2M-dot@users.noreply.github.com>
2022-02-14 20:15:18 +0300

    added new component Header

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,14 @@
-import logo from '../../logo.svg';
-import '../../App.css';
+// import logo from '../../logo.svg';
+import './App.css';
 
-function App() {
+const App = () => {
   return (
-
+    <div className="page">
+      <div className="page__container">
+        <h1>Hello</h1>
+      </div>
+    </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Logo from "./../../images/logo.svg"
+import {NavLink} from "react-router-dom";
+
+const Header = () => {
+	return (
+		<div className="header">
+			<NavLink to="/">
+				<img className="header__logo" src={Logo}/>
+			</NavLink>
+		</div>
+	);
+;
+
+export default Header;
